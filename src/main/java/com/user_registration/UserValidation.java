ebc80d7139d21214db9b4a80114c06e531f6ff1f
anubhav230 <anubhavsingh230@gmail.com>
2020-06-19 03:06:09 +0530

    ADDED-email patter for valid email

--- src/main/java/com/user_registration/UserValidation.java
+++ src/main/java/com/user_registration/UserValidation.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
     public static final String namePattern = "^[A-Z][a-z]{2,}";
+    public static final String emailPattern = "^[0-9a-zA-Z]+([. _ + -][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
+
 
     public static boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(namePattern);
@@ -13,4 +15,11 @@ public class UserValidation {
         Pattern pattern = Pattern.compile(namePattern);
         return pattern.matcher(lastName).matches();
     }
+
+    //Validate pattern for emailid
+    public static boolean validEmailId(String emialId) {
+        Pattern pattern = Pattern.compile(emailPattern);
+        return pattern.matcher(emialId).matches();
+    }
+
 }
--- src/test/java/com/user_registration/UserRegistrationValidate.java
+++ src/test/java/com/user_registration/UserRegistrationValidate.java
@@ -2,40 +2,65 @@ package com.user_registration;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
 
 public class UserRegistrationValidate {
+
+    UserValidation validator = new UserValidation();
+    String validEmail[] = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
+    String invalidEmail[] = {"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com ","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+
+
     @Test
     public void giveFirstName_WhenValid_shouldReturnTrue() {
-        UserValidation validator = new UserValidation();
         boolean result = validator.validateFirstName("Anubhav");
         Assert.assertEquals(true, result);
     }
     @Test
     public void giveFirstName_WhenSort_shouldReturnFalse() {
-        UserValidation validator = new UserValidation();
         boolean result = validator.validateFirstName("An");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void giveFirstName_WhenInvalid_shouldReturnFalse() {
-        UserValidation validator = new UserValidation();
         boolean result = validator.validateFirstName("sdf#sdf");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void giveLastName_WhenValid_shouldReturnTrue() {
-        UserValidation validator = new UserValidation();
         boolean result = validator.validateLastName("Singh");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void giveLastName_WhenSort_shouldReturnFalse() {
-        UserValidation validator = new UserValidation();
         boolean result = validator.validateLastName("Si");
         Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmailId_WhenInvalid_ShouldReturnFalse()
+    {
+        for (String email : invalidEmail)
+        {
+            boolean result = validator.validEmailId(email);
+            Assert.assertEquals(false, result);
+        }
+    }
+
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue()
+    {
+        for (String email : validEmail)
+        {
+            boolean result = validator.validEmailId(email);
+            Assert.assertEquals(true, result);
+        }
     }
 
 
