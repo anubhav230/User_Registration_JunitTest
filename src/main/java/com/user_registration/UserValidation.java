a19ae0d6c8fc176498dfda8a26e52416f8a33a8a
anubhav230 <anubhavsingh230@gmail.com>
2020-06-19 13:20:42 +0530

    REFACTORED: Refatoring the code

--- src/main/java/com/user_registration/UserValidation.java
+++ src/main/java/com/user_registration/UserValidation.java
@@ -6,29 +6,53 @@ public class UserValidation {
     public static final String namePattern = "^[A-Z][a-z]{2,}";
     public static final String emailPattern = "^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$";
     public static final String mobileNumberPattern = "^[0-9]{1,3}[ ][0-9]{10}$";
-    public static final String passwordPatten = "[A-Z0-9a-z]*[!@#%^&]{1}[A-Z0-9a-z]*";
+    public static final String passwordPatten = "((?=.*[A-Z])(?=.*\\d)(?=.*[#$@]){1}.{8})$";
 
+    /**
+     *
+     * @param firstName
+     * @return
+     */
     public static boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(namePattern);
         return pattern.matcher(firstName).matches();
     }
+
+    /**
+     *
+     * @param lastName
+     * @return
+     */
     public static boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile(namePattern);
         return pattern.matcher(lastName).matches();
     }
 
-    //Validate pattern for emailid
+    /**
+     *
+     * @param emialId
+     * @return
+     */
     public static boolean validEmailId(String emialId) {
         Pattern pattern = Pattern.compile(emailPattern);
         return pattern.matcher(emialId).matches();
     }
-    //Validate pattern for mobile number
+
+    /**
+     *
+      * @param mobileNumber
+     * @return
+     */
     public static boolean validMobileNumber(String mobileNumber) {
         Pattern pattern = Pattern.compile(mobileNumberPattern);
         return pattern.matcher(mobileNumber).matches();
     }
 
-    //Validate pattern for Password
+    /**
+     *
+     * @param password
+     * @return
+     */
     public static boolean validPassword(String password) {
         Pattern pattern = Pattern.compile(passwordPatten);
         return pattern.matcher(password).matches();
--- src/test/java/com/user_registration/UserRegistrationValidate.java
+++ src/test/java/com/user_registration/UserRegistrationValidate.java
@@ -8,7 +8,7 @@ public class UserRegistrationValidate {
 
     UserValidation validator = new UserValidation();
 
-    String validEmail[] = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
+    String validEmail[] = {"anubhavsingh230@gmail.com","abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
     String invalidEmail[] = {"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com ","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
 
 
